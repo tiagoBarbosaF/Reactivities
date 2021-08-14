c76921e29b45445cfecc195f926372b99e1625a3
Tiago Barbosa <tiago@farias.cc>
2021-08-14 20:52:44 -0300

    remove tests

--- API/Program.cs
+++ API/Program.cs
@@ -19,7 +19,6 @@ namespace API
             var host = CreateHostBuilder(args).Build();
             using var scope = host.Services.CreateScope();
             var services = scope.ServiceProvider;
-            //test again
 
             try
             {
