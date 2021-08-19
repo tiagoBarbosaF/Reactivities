e5bc29e4eb26e8e4d3b4678cd3c4a3100d44c5d9
Tiago Barbosa <tiago@farias.cc>
2021-08-18 21:28:58 -0300

    Activities controls, Create, Edit and Details

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -19,7 +19,21 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+
+            return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
         }
     }
 }
