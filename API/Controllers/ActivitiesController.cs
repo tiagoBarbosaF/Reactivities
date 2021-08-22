45f5cb26d814e1a9398427c9dbaebce04e96ecaf
Tiago Barbosa <tiago@farias.cc>
2021-08-22 17:42:44 -0300

    Created crud application for solution, modify front, applying dashboards, lists and grids

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -15,7 +15,7 @@ namespace API.Controllers
         {
             return await Mediator.Send(new List.Query());
         }
-
+        
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
