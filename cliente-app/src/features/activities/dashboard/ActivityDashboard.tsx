deedd1b1591fd225e11d3395bbbeadd6ac6f4c61
Tiago Barbosa <tiago@farias.cc>
2021-08-23 21:48:00 -0300

    Setting axios features, applying loading indicators on buttons and posting and deleting data to the server

--- cliente-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ cliente-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,6 +15,7 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({
@@ -26,19 +27,22 @@ export default function ActivityDashboard({
                                               openForm,
                                               closeForm,
                                               createOrEdit,
-                                              deleteActivity
+                                              deleteActivity,
+                                              submitting
                                           }: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity}/>
+                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity}
+                              submitting={submitting}/>
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
                 <ActivityDetails activity={selectedActivity} cancelSelectActivity={cancelSelectActivity}
                                  openForm={openForm}/>}
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}
+                              submitting={submitting}/>}
             </Grid.Column>
         </Grid>
     )
